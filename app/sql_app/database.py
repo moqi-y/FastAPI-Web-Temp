f04b139d12cb2d76a180e3251454ce443dd8b99a
yuanzhen <yuanzhenwork@qq.com>
2024-04-01 10:24:40 +0800

    feat: 调整模板

--- app/sql_app/database.py
+++ app/sql_app/database.py
@@ -3,5 +3,7 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 
 # 连接数据库
-engine = create_engine("mysql+pymysql://root:123456@localhost:3306/fastapi_web_temp", echo=False)
+# engine = create_engine("mysql+pymysql://root:123456@localhost:3306/fastapi_web_temp", echo=False)
+# sqlite
+engine = create_engine("sqlite:///./fastapi_web_temp.db", echo=False)
 Base = sqlalchemy.orm.declarative_base()
